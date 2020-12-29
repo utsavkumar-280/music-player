43773ac3ce4ea9a0e0958581e0f36b7d113aa445
Utsav Kumar <66728108+youteeeee@users.noreply.github.com>
2020-12-29 14:24:24 +0530

    feat: added currentTime and Duration

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPlay,
@@ -7,7 +7,16 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 const Player = ({ currentSong, isPlaying, setisPlaying }) => {
+  //Ref
   const audioRef = useRef(null);
+
+  //State
+  const [songInfo, setSongInfo] = useState({
+    currentTime: null,
+    duration: null,
+  });
+
+  //Event Handlers
   const playsongHandler = () => {
     if (isPlaying) {
       audioRef.current.pause();
@@ -18,12 +27,25 @@ const Player = ({ currentSong, isPlaying, setisPlaying }) => {
       setisPlaying(!isPlaying);
     }
   };
+
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({ ...songInfo, currentTime: current, duration: duration });
+  };
+
+  const getTime = (time) => {
+    return (
+      Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+    );
+  };
+
   return (
     <div className="player">
       <div className="time-control">
-        <p>Start time</p>
+        <p>{getTime(songInfo.currentTime)} </p>
         <input type="range" />
-        <p>End time</p>
+        <p>{getTime(songInfo.duration)} </p>
       </div>
       <div className="play-control">
         <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
@@ -39,7 +61,12 @@ const Player = ({ currentSong, isPlaying, setisPlaying }) => {
           icon={faAngleRight}
         />
       </div>
-      <audio ref={audioRef} src={currentSong.audio}></audio>
+      <audio
+        onLoadedMetadata={timeUpdateHandler}
+        onTimeUpdate={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+      ></audio>
     </div>
   );
 };
--- src/util.js
+++ src/util.js
@@ -10,7 +10,7 @@ function Chillpop() {
       audio: "https://mp3.chillhop.com/serve.php/?mp3=7976",
       id: uuidv4(),
       color: ["#0E4466", "#77CBDE"],
-      active: true,
+      active: false,
     },
     {
       name: "Swimming",
@@ -60,6 +60,76 @@ function Chillpop() {
       audio: "https://mp3.chillhop.com/serve.php/?mp3=10027",
       id: uuidv4(),
       color: ["#273A5A", "#F9EEDA"],
+      active: false,
+    },
+    {
+      name: "Flushing the Stairs",
+      artist: "Leavv",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/88e7eb711f8c71d87fc102e97cf91e36f692348d-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9917",
+      id: uuidv4(),
+      color: ["#184B51", "#DB9066"],
+      active: false,
+    },
+    {
+      name: "Hibiscus",
+      artist: "Felty",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/858b533ba20ff95bf5b401089b195d1a8cb43870-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=8971",
+      id: uuidv4(),
+      color: ["#14285A", "#EA9D45"],
+      active: false,
+    },
+    {
+      name: "Unlisted",
+      artist: "Amparo",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/efaa848553d09b4d9fc0752844456e41b34de276-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9232",
+      id: uuidv4(),
+      color: ["#563D72", "#65BAFD"],
+      active: false,
+    },
+    {
+      name: "Cold Outside",
+      artist: "Nymano",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/09fb436604242df99f84b9f359acb046e40d2e9e-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10150",
+      id: uuidv4(),
+      color: ["#595383", "#F6A5A1"],
+      active: false,
+    },
+    {
+      name: "fiveyearsago",
+      artist: "Psalm Trees",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/05/861564cb8a53229de4e63541a44f507c8b0da6ec-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9021",
+      id: uuidv4(),
+      color: ["#124E2A", "#C52D2B"],
+      active: false,
+    },
+    {
+      name: "Dove",
+      artist: "Misha",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/05/74aa978ba66ea997cbf6fb5f451878e121bf0728-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9326",
+      id: uuidv4(),
+      color: ["#124E2A", "#FB9026"],
+      active: false,
+    },
+    {
+      name: "Cloud Carpets",
+      artist: "The Field Tapes, Ezzy, Wowflower",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/11/8a0c857ddad531279d0757f5362380a6837b1b69-1024x1024.jpg",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=11248",
+      id: uuidv4(),
+      color: ["#46659C", "#BFCB67"],
       active: false,
     },
   ];
